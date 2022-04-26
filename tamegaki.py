5388c93a700a14d966869b4b31d4e364d3e6fc40
yukihoz <100668691+yukihoz@users.noreply.github.com>
2022-04-27 00:29:01 +0900

    Update tamegaki.py

--- tamegaki.py
+++ tamegaki.py
@@ -28,10 +28,20 @@ st.markdown('''
 )
 
 st.markdown('''
-    <a href="https://twitter.com/intent/tweet?text=つまずいてるイラスト%20https://twitter.com/ninofku" target="_blank">この画像をツイートする
-    </a>''',
+    <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="false">Tweet</a>
+<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+    ''',
         unsafe_allow_html=True
 )
+
+
+
+st.markdown('''
+    <a href="https://twitter.com/intent/tweet?text=フローレンスつまずいてるイラスト%20https://florence.or.jp%20pic.twitter.com/FQ5xUkhaAAAJBIC%20@nomarktanaka" target="_blank">この画像をツイートする</a>
+    ''',
+        unsafe_allow_html=True
+)
+
 
 st.markdown('''
     <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
